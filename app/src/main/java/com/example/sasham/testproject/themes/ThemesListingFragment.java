3d94d8621d945263c9b7184620e1ca8a04ec27ab
Sasha <mazur.sasha.moo@gmail.com>
2018-07-28 18:29:14 +0300

    Create Theme model

--- app/src/main/java/com/example/sasham/testproject/themes/ThemesListingFragment.java
+++ app/src/main/java/com/example/sasham/testproject/themes/ThemesListingFragment.java
@@ -4,13 +4,20 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.sasham.testproject.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ThemesListingFragment extends Fragment {
+
+    @BindView(R.id.themes_listing_recycler_view)
+    public RecyclerView themesRecyclerView;
 
     public ThemesListingFragment() {
         // Required empty public constructor
@@ -26,7 +33,12 @@ public class ThemesListingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_themes_listing, container, false);
+        View root= inflater.inflate(R.layout.fragment_themes_listing, container, false);
+
+        //Init views
+        ButterKnife.bind(this,root);
+
+        return root;
     }
 
 
