10e01a78a3a3b5fe2b1eb47ed698a6ab1c4cdfd4
Sasha <mazur.sasha.moo@gmail.com>
2018-07-28 17:37:00 +0300

    Create ThemesActivity

--- app/src/main/java/com/example/sasham/testproject/themes/ThemesActivity.java
+++ app/src/main/java/com/example/sasham/testproject/themes/ThemesActivity.java
@@ -1,13 +1,15 @@
-package com.example.sasham.testproject;
+package com.example.sasham.testproject.themes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.sasham.testproject.R;
+
+public class ThemesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_themes);
     }
 }
