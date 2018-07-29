6528282373b56be8d5fd0d468af012e6a8d37ca3
Sasha <mazur.sasha.moo@gmail.com>
2018-07-29 20:34:02 +0300

    Show progress when loading web page

--- app/src/main/java/com/example/sasham/testproject/website/WebActivity.java
+++ app/src/main/java/com/example/sasham/testproject/website/WebActivity.java
@@ -1,10 +1,13 @@
 package com.example.sasham.testproject.website;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.example.sasham.testproject.R;
 
@@ -18,6 +21,9 @@ public class WebActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+
+    @BindView(R.id.progress)
+    ProgressBar progress;
 
     public static final String WEB_URL_ARGS = "url";
 
@@ -42,7 +48,21 @@ public class WebActivity extends AppCompatActivity {
     private void openWebSite() {
         String url=getIntent().getStringExtra(WEB_URL_ARGS);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progress.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progress.setVisibility(View.GONE);
+            }
+        });
+
         webView.loadUrl(url);
         setTitle(url);
     }
